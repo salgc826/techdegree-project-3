e85f106563691ddbe3b091da3d0a71f8f9edad8c
salgc826 <csalgado307@gmail.com>
2017-12-09 18:02:04 -0800

    add textfield

--- js/main.js
+++ js/main.js
@@ -14,3 +14,15 @@ $(document).ready(function() {
   $('#colors-js-puns').hide();
   $('.emailErrorMessage').hide();
 });
+
+//A text field that will be revealed when the "other" option is selected from the "Job Role" drop down menu
+document.getElementById('title').onchange = function() {
+  //get the selected text
+  const selectedValue = this.selectedIndex;
+  const inputText = this.children[selectedValue].innerHTML.trim();
+  //if selected text is
+  if (inputText === 'Other') {
+    //add textfield
+    $('.other-jobrole-fieldset').show();
+  }
+};
