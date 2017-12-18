66551d441b1a9f29685a9d0f2fc980353a20e008
Carlos Salgado <csalgado307@gmail.com>
2017-12-18 13:06:36 -0800

    validation form 5

--- js/main.js
+++ js/main.js
@@ -135,127 +135,111 @@ $("#payment").change(function(){
 	}
 });
 
-// Form cannot be submiited until the following requirements have been met:
-//	1. Name field isn't blank
-//	2. Email field contains validly formatted email address
-//	3. At least one checkbox under "register for Activities" section must be selected
-//	4. If "Credit Card" is the selected payment option, the three fields accept only numbers, a 16-digit credit card number, a 5-digit zip code,
-//     and a 3-digit CVV
+// button mousedown function which checks validation
+// now this, this I like this solution
+$("button").click(function(event) {
+	isFormValid(event);
+});
 
-/*Validation*/
-//functions that validate the name field
-function checkName(){
-  if($("#name").val().length < 1){
-    $("#name").addClass("warning");
-  }else{
-    $("#name").removeClass("warning");
-  }
+// checks the validity of the form
+function isFormValid(event) {
+	resetFormColors();
+	var invalidateForm = false;
+	if (!nameEntered()) {
+		$("label[for='name']").text("Name: (Please provide your name)").css("color", "red");
+		invalidateForm = true;
+	}
+	if (!validEmailAddress()) {
+		$("label[for='mail']").text("Email: (Please provide a valid email address)").css("color", "red");
+		invalidateForm = true;
+	}
+	if (!activitySelected()) {
+		$(".activities legend").css("color", "red");
+		invalidateForm = true;
+	}
+	if (!teeShirtSelected()) {
+		console.log("invalide tee-shirt");
+		console.log($("#design").val());
+		$(".shirt legend").append("<p>Don't forget to pick a T-Shirt</p>");
+		$(".shirt legend p").css("color", "red");
+		invalidateForm = true;
+	}
+	if ($("#payment").val() === "select_method") {
+		$("fieldset:last legend").css("color", "red");
+		invalidateForm = true;
+	}
+	if ($("#payment").val() === "credit card") {
+		if(!validCreditCard($("#cc-num").val()) || $("#cc-num").val() === "") {
+			$("#credit-card label[for='cc-num']").css("color", "red");
+			invalidateForm = true;
+		}
+		if (!ccvAndZipEntered()) {
+			$("#credit-card label[for='zip']").css("color", "red");
+			$("#credit-card label[for='cvv']").css("color", "red");
+			invalidateForm = true;
+		}
+	}
+	if (invalidateForm) {
+		event.preventDefault();
+	}
 }
 
-$("#name").focus(function(){
-  $("#name").removeClass("noFirstInput");
-  checkName();
-});
-$("#name").change(function(){
-  checkName();
-});
+// function which checks if a name has been entered
+function nameEntered() {
+	return ($("#name").val() !== "") ? true : false;
+}
 
-//functions that validate the email field using a regEx found on internet.
-function checkMail(){
-  var string = $("#mail").val();
-  var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-  //console.log(pattern.test(string));
-  if(pattern.test(string)){
-    //console.log(pattern.test(string));
-    $("#mail").removeClass("noFirstInput");
-    $("#mail").removeClass("warning");
-  }else{
-    $("#mail").addClass("warning");
-  }
-};
+// function which checks the validity of the email address
+function validEmailAddress() {
+	var validEmail = new RegExp(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i);
+	return validEmail.test($("#mail").val());
+}
 
-$("#mail").change(function(){
-  checkMail();
-});
-$("#mail").focus(function(){
-  $("#mail").addClass("warning");
-  checkMail();
-});
+// function which checks to see if a tee-shirt has been selected .
+function teeShirtSelected() {
+		return (!($("#design").val() == "Select Theme")) ? true : false;
+}
 
-//functions that validate the checkboxes
-function checkCheckbox(){
-  if($(".activities input:checked").length > 0){
-    $(".activities").removeClass("noFirstInput");
-    $(".activities legend").removeClass("warning");
-    $(".activities legend").css("color", "#184f68");
-  }else{
-    $(".activities legend").css("color", "red");
-    $(".activities legend").addClass("noFirstInput");
-  }
-};
+// function which determines if an activity has been selected by user
+function activitySelected() {
+	var selectedActivityCount = 0;
+	$.each($("input[type='checkbox']"), function() {
+		if ($(this).prop("checked")) {
+			selectedActivityCount += 1;
+		}
+	});
+	return (selectedActivityCount > 0) ? true : false;
+}
 
-$(".activities").change(function(){
-  checkCheckbox();
-});
+// function which determins if a zip code and 3 digit ccv number has been selected
+// I didn't validate these, I could but it's time to move on to the next project, and it's an unrequested feature.
+function ccvAndZipEntered() {
+	var zipVal = /^\d{5}$|^\d{5}-\d{4}$/;
+	var cvvVal = /^\d{3}$/;
+	return zipVal.test($("#zip").val()) && cvvVal.test($("#cvv").val());
+}
 
-//functions that validate the credit card part of the form
-function checkCC(){
-  if($("#cc-num").val().length < 13 || $("#cc-num").val().length > 16){
-    $("#cc-num").addClass("warning");
-  }else{
-    $("#cc-num").removeClass("warning");
-  }
-};
-$("#cc-num").change(function(){
-  checkCC();
-});
-$("#cc-num").focus(function(){
-  checkCC();
-});
-
-//functions that validate the credit zip code of the form
-function checkZip(){
-  if($("#zip").val().length === 5){
-    $("#zip").removeClass("warning");
-  }else{
-    $("#zip").addClass("warning");
-  }
-};
-$("#zip").change(function(){
-  checkZip();
-});
-$("#zip").focus(function(){
-  checkZip();
-});
-//functions that validate the cvv part of the form
-function checkCVV(){
-  if($("#cvv").val().length === 3){
-    $("#cvv").removeClass("warning");
-  }else{
-    $("#cvv").addClass("warning");
-  }
-};
-$("#cvv").change(function(){
-  checkCVV();
-});
-$("#cvv").focus(function(){
-  checkCVV();
-});
-
+// functioin which checks the validity of the credit card number entered
+// pulled this from DiegoSalazar on github. I don't need to reinvent the wheel
+function validCreditCard(value) {
+  // accept only digits, dashes or spaces
+	if (/[^0-9-\s]+/.test(value)) return false;
+	// The Luhn Algorithm.
+	var nCheck = 0, nDigit = 0, bEven = false;
+	value = value.replace(/\D/g, "");
+	for (var n = value.length - 1; n >= 0; n--) {
+		var cDigit = value.charAt(n),
+			  nDigit = parseInt(cDigit, 10);
+		if (bEven) {
+			if ((nDigit *= 2) > 9) nDigit -= 9;
+		}
+		nCheck += nDigit;
+		bEven = !bEven;
+	}
+	return (nCheck % 10) === 0;
+}
 
 // function which resets the form colors on submission, so they are black if corrected
-function resetFormColors() {
-	$(".shirt legend p").remove();
-	$("label[for='name']").text("Name:").css("color", "black");
-	$("label[for='mail']").text("Email:").css("color", "black");
-	$(".activities legend").css("color", "black");
-	$("fieldset:last legend").css("color", "black");
-	$("#credit-card label[for='cc-num']").css("color", "black");
-	$("#credit-card label[for='zip']").css("color", "black");
-	$("#credit-card label[for='cvv']").css("color", "black");
-}
-
-// resets the form colors on submission, so they are black if corrected
 function resetFormColors() {
 	$(".shirt legend p").remove();
 	$("label[for='name']").text("Name:").css("color", "black");
