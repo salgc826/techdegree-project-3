1939ed549e73d552ac562f7ed42051310dfcbead
Carlos Salgado <csalgado307@gmail.com>
2017-12-18 10:24:53 -0800

    reset form validation

--- js/main.js
+++ js/main.js
@@ -143,166 +143,115 @@ $("#payment").change(function(){
 //     and a 3-digit CVV
 
 /*Validation*/
-//Check if credit card number is valid or not with the Luhn check algorithm
-function checkcc(number) {
-  var len = number.length,
-  mul = 0,
-  prodArr = [
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
-  ],
-  sum = 0;
-
-  while (len--) {
-    sum += prodArr[mul][parseInt(number.charAt(len), 10)];
-    mul ^= 1;
-  }
-
-  return sum % 10 === 0 && sum > 0;
+// checks the validity of the form
+function isFormValid(event) {
+	resetFormColors();
+	var invalidateForm = false;
+	if (!nameEntered()) {
+		$("label[for='name']").text("Name: (Please provide your name)").css("color", "red");
+		invalidateForm = true;
+	}
+	if (!validEmailAddress()) {
+		$("label[for='mail']").text("Email: (Please provide a valid email address)").css("color", "red");
+		invalidateForm = true;
+	}
+	if (!activitySelected()) {
+		$(".activities legend").css("color", "red");
+		invalidateForm = true;
+	}
+	if (!teeShirtSelected()) {
+		console.log("invalide tee-shirt");
+		console.log($("#design").val());
+		$(".shirt legend").append("<p>Don't forget to pick a T-Shirt</p>");
+		$(".shirt legend p").css("color", "red");
+		invalidateForm = true;
+	}
+	if ($("#payment").val() === "select_method") {
+		$("fieldset:last legend").css("color", "red");
+		invalidateForm = true;
+	}
+	if ($("#payment").val() === "credit card") {
+		if(!validCreditCard($("#cc-num").val()) || $("#cc-num").val() === "") {
+			$("#credit-card label[for='cc-num']").css("color", "red");
+			invalidateForm = true;
+		}
+		if (!ccvAndZipEntered()) {
+			$("#credit-card label[for='zip']").css("color", "red");
+			$("#credit-card label[for='cvv']").css("color", "red");
+			invalidateForm = true;
+		}
+	}
+	if (invalidateForm) {
+		event.preventDefault();
+	}
 }
 
-//Program at least one of your error messages so that more information is provided depending on the error.
-//This function does 4 different checks on submit and displays a custom error message.
-//To test all of the error messages out, try submitting the form with the following expressions: " ", "@", "123456", "510510510510510"
-//And finally, try these valid card numbers: "5105105105105100", "4111111111111111", "4012888888881881" or even your own card number!
-function validateCC() {
-  var ccnum = $("#cc-num").val();
-  if(ccnum.length < 1) {
-      $("label[for=cc-num]").text("Card Number: Can't be blank.").css("color", "red");
-  } else if (/[^0-9]+/.test(ccnum)) {
-    $("label[for=cc-num]").text("Card Number: Please enter digits only.").css("color", "red");
-  } else if (ccnum.length < 13 || ccnum.length > 16) {
-    $("label[for=cc-num]").text("Card Number: Must be between 13 and 16 digits.").css("color", "red");
-  } else if (!checkcc(ccnum)) {
-    $("label[for=cc-num]").text("Card Number: Your card number is invalid.").css("color", "red");
-  } else {
-    $("label[for=cc-num]").text("Card Number:").css("color", "black");
-    return true;
-  }
+// function which checks if a name has been entered
+function nameEntered() {
+	return ($("#name").val() !== "") ? true : false;
 }
 
-//Live email onkeyup verification based on the regex before submitting the form.
-//If the field is not empty and it does not pass the regex test, display error message. Otherwise, put default styling.
-  $("#mail").keyup(function(){
-  var email = $("#mail").val();
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  if ($("#mail").val().length > 1 && regex.test(email) ) {
-      $("label[for=mail").text("Email:").css("color", "black");
-  } else {
-      $("label[for=mail").text("Email: (please provide a valid email address)").css("color", "red");
-  }
-});
-
-//Give properties to each one of the fields and if any of them are wrong or empty, then prevent the form submission.
-function validateForm() {
-
-  //Name field can't be blank
-  function validateName() {
-  if ($("#name").val().length < 1) {
-    $('html, body').animate({
-      scrollTop: $("label[for=name]").offset().top
-    }, 2000);
-      $("label[for=name]").text("Name: (please provide your name)").css("color", "red");
-      $("input:text:visible:first").focus();
-    } else {
-      $("label[for=name]").text("Name:").css("color", "");
-      return true;
-    }
-  } validateName();
-
-  //In the name field onkeyup, display or remove error message.
-  $("#name").keyup(function(){
-  if ($("#name").val().length > 0) {
-    $("label[for=name]").text("Name:").css("color", "");
-    return true;
-  } else {
-    $("label[for=name]").text("Name: (please provide your name)").css("color", "red");
-  }
-  });
-
-//Validate email when you submit the form by passing the regex test.
-  function validateEmail() {
-    var email = $("#mail").val();
-    if ($("#mail").val().length > 1) {
-      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-     if (regex.test(email)) {
-        $("label[for=mail").text("Email:").css("color", "black");
-        return true;
-      }
-    } else {
-        $("label[for=mail").text("Email: (please provide a valid email address)").css("color", "red");
-    }
-  } validateEmail();
-
-  //At least one activity must be checked from the list under "Register for Actitivities."
-  function validateActivities() {
-    if ($("input:checkbox:checked").length < 1) {
-        $(".activities legend").text("Register for activities: (please select an activity)").css("color", "red");
-    } else {
-          $(".activities legend").text("Register for activities:").css("color", "");
-          return true;
-    }
-  } validateActivities();
-
-//T-Shirt theme must be selected. If not, throw error.
-  function validateShirtInfo() {
-    if($("#design").val() === "Select Theme") {
-        $("#validateShirt").remove();
-        $(".shirt legend").append("<p id='validateShirt' style='font-size : 15px'>Please choose your T-Shirt</p>").css("color", "#ff0000");
-    } else {
-       $("#validateShirt").remove();
-       $(".shirt legend").css("color", "#000");
-       return true;
-    }
-  } validateShirtInfo();
-
-//Payment option must be selected.
-  function validatePaymentOption() {
-  if ($('#payment').val() === "select_method"){
-      $("label[for=payment]").text("I'm going to pay with: Please select a payment option.").css("color", "red");
-  } else {
-      $("label[for=payment]").text("I'm going to pay with:").css("color", "black");
-      return true;
-    }
-} validatePaymentOption();
-//Must enter a valid zip number of 5 digits. The regex says "digits only"
-  function validateZip() {
-    var zipval = $("#zip").val();
-    if (zipval.length !== 5 || /\D+/g.test(zipval)) {
-      $("label[for=zip]").text("Zip Code: Invalid Zip").css({'color': 'red', 'font-size': '15px'});
-    } else {
-      $("label[for=zip]").text("Zip Code:").css("color", "black");
-      return true;
-    }
-  } validateZip();
-//Must enter a valid CVV number of 3 digits. The regex says "digits only"
-  function validateCVV() {
-    var cvvval = $("#cvv").val();
-    if (cvvval.length !== 3 || /\D+/g.test(cvvval)) {
-      $("label[for=cvv]").text("CVV: Invalid CVV").css({'color': 'red', 'font-size': '15px'});
-    } else {
-      $("label[for=cvv]").text("CVV:").css("color", "black");
-      return true;
-    }
-  } validateCVV();
-
-//Return True if all fields are correct, else return false to prevent the submission of the form
-var paymentVal = $("#payment").val();
-  if (paymentVal === "credit card" && validateCVV() && validateZip() && validateCC() && validateShirtInfo() && validateActivities() && validateEmail() && validateName()) {
-    return true;
-  } else if (paymentVal === "bitcoin" || paymentVal === "paypal" && validateShirtInfo() && validateActivities() && validateEmail() && validateName()) {
-    return true;
-  } else {
-    return false;
-  }
+// function which checks the validity of the email address
+function validEmailAddress() {
+	var validEmail = new RegExp(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i);
+	return validEmail.test($("#mail").val());
 }
 
-//On sumbmit, prevent default and call all the form validation functions.
-$("form").submit(function(e){
-  if(!validateForm()) {e.preventDefault();}
-      validateForm();
-      validateCC();
-  });
+// function which checks to see if a tee-shirt has been selected .
+function teeShirtSelected() {
+		return (!($("#design").val() == "Select Theme")) ? true : false;
+}
+
+// function which determines if an activity has been selected by user
+function activitySelected() {
+	var selectedActivityCount = 0;
+	$.each($("input[type='checkbox']"), function() {
+		if ($(this).prop("checked")) {
+			selectedActivityCount += 1;
+		}
+	});
+	return (selectedActivityCount > 0) ? true : false;
+}
+
+// function which determins if a zip code and 3 digit ccv number has been selected
+// I didn't validate these, I could but it's time to move on to the next project, and it's an unrequested feature.
+function ccvAndZipEntered() {
+	var zipVal = /^\d{5}$|^\d{5}-\d{4}$/;
+	var cvvVal = /^\d{3}$/;
+	return zipVal.test($("#zip").val()) && cvvVal.test($("#cvv").val());
+}
+
+// functioin which checks the validity of the credit card number entered
+// pulled this from DiegoSalazar on github. I don't need to reinvent the wheel
+function validCreditCard(value) {
+  // accept only digits, dashes or spaces
+	if (/[^0-9-\s]+/.test(value)) return false;
+	// The Luhn Algorithm.
+	var nCheck = 0, nDigit = 0, bEven = false;
+	value = value.replace(/\D/g, "");
+	for (var n = value.length - 1; n >= 0; n--) {
+		var cDigit = value.charAt(n),
+			  nDigit = parseInt(cDigit, 10);
+		if (bEven) {
+			if ((nDigit *= 2) > 9) nDigit -= 9;
+		}
+		nCheck += nDigit;
+		bEven = !bEven;
+	}
+	return (nCheck % 10) === 0;
+}
+
+// function which resets the form colors on submission, so they are black if corrected
+function resetFormColors() {
+	$(".shirt legend p").remove();
+	$("label[for='name']").text("Name:").css("color", "black");
+	$("label[for='mail']").text("Email:").css("color", "black");
+	$(".activities legend").css("color", "black");
+	$("fieldset:last legend").css("color", "black");
+	$("#credit-card label[for='cc-num']").css("color", "black");
+	$("#credit-card label[for='zip']").css("color", "black");
+	$("#credit-card label[for='cvv']").css("color", "black");
+}
 
 // resets the form colors on submission, so they are black if corrected
 function resetFormColors() {
