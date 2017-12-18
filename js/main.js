0f7bfa624cf61dbe05b3a272a2e0797e6870ab85
Carlos Salgado <csalgado307@gmail.com>
2017-12-18 09:58:12 -0800

    validation form

--- js/main.js
+++ js/main.js
@@ -142,165 +142,21 @@ $("#payment").change(function(){
 //	4. If "Credit Card" is the selected payment option, the three fields accept only numbers, a 16-digit credit card number, a 5-digit zip code,
 //     and a 3-digit CVV
 
-function is_name_field_blank() {
-	if ($("#name").val() === "") {
-		return true
-	} else {
-		return false
+/*Validation*/
+$("button").on("click", (event) => {
+	if(nameVal() && mailVal() && tShirt() && register() && creditCard()) { //resets the homepage by not prevent the default use of button / submit.
+
+	} else { //Prevents the information to be removed and tells the user what went wrong.
+		event.preventDefault();
+		nameVal();
+		mailVal();
+		tShirt();
+		register();
+		creditCard();
+
 	}
-}
-
-function is_email_format_valid() {
-	var valid_email_regex = /.+@.+\..+/
-	return valid_email_regex.test($("#mail").val());
-}
-
-function is_an_activity_selected() {
-	if ($(".activities input[type='checkbox']").is(":checked") || !$(".activities input.none[name='first']").is(":checked") || !$(".activities input.none[name='second']").is(":checked")) {
-		$(".activity-error").hide();
-		return true;
-	} else {
-		$(".activity-error").show();
-		return false;
-	}
-}
-
-function is_cc_a_number() {
-	var cc_regex = /^\d{16}$/
-	return cc_regex.test($("#cc-num").val())
-}
-
-function is_zip_a_number() {
-	var zip_regex = /^\d{5}$/
-	return zip_regex.test($("#zip").val())
-}
-
-function is_cvv_a_number() {
-	var cvv_regex = /^\d{3}$/
-	return cvv_regex.test($("#cvv").val())
-}
-
-function basic_info_entered() {
-	if (is_an_activity_selected() && is_email_format_valid() && !is_name_field_blank()) {  // these conditions are checked left to right -- their order needs to be
-		return true;																		 // reversed from the order a user typically works through the form
-	} else {
-
-		return false;  //more basic info is required
-	};
-}
-
-function cc_info_copacetic() {
-	if ($("#payment").prop("value") === "credit card") {
-		if (is_cc_a_number() && is_zip_a_number() && is_cvv_a_number()) {
-			return true				//cc info is good
-		} else {
-			return false		//something wrong with cc info
-		};
-	} else if (($("#payment").val() === "paypal") || ($("#payment").val() === "bitcoin")) {
-		return true				// no cc info needed
-	} else {
-		 						// default "select payment method" is selected, so do nothing
-	}
-}
-
-function determine_enabled() {
-	if (basic_info_entered() && cc_info_copacetic()) {
-		$("#register_btn").prop("disabled", false)
-	} else {
-		$("#register_btn").prop("disabled", true)
-	}
-}
-
-
-// by default the register button is diabled, and the email-format error is hidden
-
-$("#register_btn").prop("disabled", true);
-$("#format-error").hide();
-
-// every time one of the form elements changes, the register button may need to be enabled
-// also, validation error messages are displayed where appropriate
-
-$("#name").on("input", function () {
-
-	if ($("#name").val() === "") {
-		$("#name").next().show()
-	} else {
-		$("#name").next().hide()
-	}
-
-	determine_enabled()
 });
 
-$("#mail").on("input", function () {
-
-	if (/.+@.+\..+/.test($("#mail").val())) {
-		$("#format-error").hide();
-	} else {
-		$("#format-error").show();
-	}
-
-	if ($("#mail").val() === "") {
-		$("#format-error").hide();
-		$("#empty-error").show();
-	} else {
-		$("#empty-error").hide()
-
-	}
-
-	determine_enabled()
-});
-
-$(".activities input[type='checkbox']").on("change", function () {
-	determine_enabled()
-});
-
-$(".activities input[name='first']").on("change", function () {
-	determine_enabled()
-})
-
-$(".activities input[name='second']").on("change", function () {
-	determine_enabled()
-})
-
-$("#payment").on("change", function () {
-	determine_enabled()
-})
-
-
-$("#cc-num").on("change", function () {
-
-	if (/^\d{16}$/.test($("#cc-num").val())) {
-		$("#cc-num").next().hide();
-	} else {
-		$("#cc-num").next().show();
-	}
-
-	determine_enabled()
-})
-
-
-$("#zip").on("change", function () {
-
-	if (/^\d{5}$/.test($("#zip").val())) {
-		$("#zip").next().hide();
-	} else {
-		$("#zip").next().show();
-	}
-
-	determine_enabled()
-})
-
-
-$("#cvv").on("change", function () {
-
-	if (/^\d{3}$/.test($("#cvv").val())) {
-		$("#cvv").next().hide();
-	} else {
-		$("#cvv").next().show();
-	};
-
-	determine_enabled()
-})
 // resets the form colors on submission, so they are black if corrected
 function resetFormColors() {
 	$(".shirt legend p").remove();
